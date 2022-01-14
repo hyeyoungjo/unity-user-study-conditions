a6660b4bf818e664df78e6aa4ab18fd1ee86cd93
Hye-Young Jo <jhy.vfx@gmail.com>
2022-01-14 10:41:09 +0900

    ShowOnly attribute

---
+++ Assets/Scripts/GetStudyOrder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetStudyOrder : MonoBehaviour
+{
+    [ShowOnly]
+    public string test="test";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
