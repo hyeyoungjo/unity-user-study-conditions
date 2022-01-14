a966a1dc3929f2bdd8f9dd3daca54ade036aaabd
Hye-Young Jo <jhy.vfx@gmail.com>
2022-01-14 10:55:13 +0900

    get study order from csv file

--- Assets/Scripts/GetStudyOrder.cs
+++ Assets/Scripts/GetStudyOrder.cs
@@ -1,19 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
 
 public class GetStudyOrder : MonoBehaviour
 {
-    [ShowOnly]
-    public string test="test";
-    void Start()
+    // Set condition in cs file "Assets/StudyCondition.cs"
+    // Set order in csv file "Assets/StudyOrder.csv"
+    // -----------------------SET CONDITION HERE--------------------------
+    [Header("[File]")]
+    [ShowOnly] [SerializeField] private string file_name = "StudyOrder";
+
+
+    [Header("[Participant]")]
+    [Tooltip("Write Participant Number, e.g. 'P1'")]
+    public string participant = "P1"; 
+    //public List<string> orderList = new List<string>();
+    [ShowOnly] public string[] orderArr;
+
+
+    void Awake()
     {
-        
+        ReadCSVFile(file_name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReadCSVFile(string fileName)
     {
-        
+        string filePath = Application.dataPath;
+        //string fileName = "LocationOrder";
+        StreamReader strReader = new StreamReader(filePath + "/" + fileName + ".csv");
+        bool endOfFile = false;
+        while (!endOfFile)
+        {
+            string data_String = strReader.ReadLine();
+            if(data_String == null)
+            {
+                endOfFile = true;
+                break;
+            }
+
+            var dataLines = data_String.Split('\n');
+            //Debug.Log(dataLines.ToString());
+             for(int i=0; i<dataLines.Length; i++)
+             {
+                 var dataLine = dataLines[i].Split(',');
+
+                 //if participant number matches, get order
+                 //print(dataLine[0].ToString());
+                 if(dataLine[0].ToString() == participant)
+                 {
+                    List<string> loadedOrderList = new List<string>();
+                    for(int d=0; d <dataLine.Length; d++)
+                    {
+                        string data = dataLine[d];
+                        loadedOrderList.Add(data);
+                    }
+                    string[] loadedOrderArr = loadedOrderList.ToArray();
+                    //orderList = loadedOrderList; //list
+                    orderArr = loadedOrderArr; //array
+                 }
+             }
+        }
     }
 }
---
+++ Assets/StudyCondition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Define study name and corresponding numeric value in the 'StudyCondition.csv' file.
+// Then, write down study order in the 'StudyOrder.csv' file.
+
+public enum LocationCondition 
+{
+    Default=0, // always write default
+    FirstCondition=1, 
+    SecondCondition=2,
+    ThirdCondition=3, 
+    FourCondition=4
+};
+
