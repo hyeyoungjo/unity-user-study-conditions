c4a0f7f5e995362553e1f225a65ed135444b501e
Hye-Young Jo <jhy.vfx@gmail.com>
2022-01-14 12:33:04 +0900

    assign sample condition function

--- Assets/Scripts/ChangeCondition.cs
+++ Assets/Scripts/ChangeCondition.cs
@@ -6,6 +6,10 @@ public class ChangeCondition : MonoBehaviour
 {
     private StudyCondition target_condition;
 
+    // -----------------------SAMPLE--------------------------
+    [Header("[Automatic]")]
+    public List<GameObject> myObjs;
+
     void OnEnable()
     {
         SetStudyOrder.OnChangeCondition += SwitchCondition;
@@ -54,23 +58,41 @@ public class ChangeCondition : MonoBehaviour
     void DefaultFunc()
     {
         //Debug.Log("DefaultFunc");
+        if(myObjs.Count != 0)
+        {
+            foreach(GameObject obj in myObjs)
+            {
+                obj.SetActive(false);
+            }
+        }
     }
+
 
     void FirstFunc()
     {
         //Debug.Log("FirstFunc");
+        try{myObjs[0].SetActive(true);}
+        catch(System.Exception e){Debug.Log("Error. Missing GameObject in First Condition");}
     }
+
     void SecondFunc()
     {
         //Debug.Log("SecondFunc");
+        try{myObjs[1].SetActive(true);}
+        catch(System.Exception e){Debug.Log("Error. Missing GameObject in Second Condition");}
     }
+    
     void ThirdFunc()
     {
         //Debug.Log("ThirdFunc");
+        try{myObjs[2].SetActive(true);}
+        catch(System.Exception e){Debug.Log("Error. Missing GameObject in Third Condition");}
     }
     void FourthFunc()
     {
         //Debug.Log("FourthFunc");
+        try{myObjs[3].SetActive(true);}
+        catch(System.Exception e){Debug.Log("Error. Missing GameObject in Fourth Condition");}
 
     }
 }
